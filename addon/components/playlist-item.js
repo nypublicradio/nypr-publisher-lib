026162b07b2783cc4f8deca45ca6f3576e34f482
Diana Parrington <diana.parrington@gmail.com>
2017-03-31 13:12:13 -0400

    playlist page edits

--- addon/components/playlist-item.js
+++ addon/components/playlist-item.js
@@ -2,5 +2,10 @@ import Ember from 'ember';
 import layout from '../templates/components/playlist-item';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+  /* entry comes in as seconds, convert to ms */
+  entryLengthMilliseconds: Ember.computed('item.catalogEntry.length', function() {
+    return this.get('item.catalogEntry.length') * 1000;
+  }),
+ 
 });
