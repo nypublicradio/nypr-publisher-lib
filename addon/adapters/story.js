20ea678987d7a639468f78df0df7459b09c66fce
Brian Whitton <me@brianwhitton.com>
2017-08-18 18:07:21 -0400

    this was the serializer, not the adapter

--- addon/adapters/story.js
+++ addon/adapters/story.js
@@ -1,33 +1,49 @@
+import config from 'ember-get-config';
 import DS from 'ember-data';
-import { camelizeObj } from 'nypt-publisher-lib/helpers/camelize-object';
+import wrapAjax from 'nypr-publisher-lib/utils/wrap-ajax';
+// TODO: auth headers for native fetch
+// import fetch from 'fetch';
 
-//dasherized versions of names in model bc they haven't been processsed yet
-const propertiesWithChildren = [
-  'appearances',
-  'chunks',
-  'headers',
-  'image-main',
-  'playlist',
-  'producing-organizations',
-  'segments',
-  'series',
-  'show-producing-orgs',
-  'slideshow'
-];
 
-export default DS.JSONAPISerializer.extend({
-  extractId: (modelClass, {attributes}) => attributes.slug,
+const DRAFT_TOKENS = ['content_type_id', 'object_id', 'token', '_']
 
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-
-    for (var prop of propertiesWithChildren) {
-      //if we have the property, process it
-      if (payload.data.attributes && payload.data.attributes.hasOwnProperty(prop)){
-        payload.data.attributes[prop] = camelizeObj(payload.data.attributes[prop]);
-      }
-    }
-
-    return this._super(store, primaryModelClass, payload, id, requestType);
+export default DS.JSONAPIAdapter.extend({
+  authorizer: 'authorizer:nypr',
+  host: config.wnycAPI,
+  namespace: 'api/v3',
+  pathForType: () => 'story',
+  buildURL() {
+    return this._super(...arguments) + '/';
   },
-
+  buildQuery({ adapterOptions: { queryParams = [] } = {}}) {
+    let query = this._super(...arguments);
+    if (Object.keys(queryParams).includes(...DRAFT_TOKENS)) {
+      Object.keys(queryParams).forEach(k => query[k] = queryParams[k]);
+    }
+    return query;
+  },
+  urlForFindRecord(id) {
+    if (/^\d+$/.test(id)) {
+      return `${this.host}/${this.namespace}/story-pk/${id}`;
+    } else {
+      return this._super(...arguments);
+    }
+  },
+  query(store, type, {related}) {
+    if (related) {
+      let url = `${this.host}/${this.namespace}/story/related/?limit=${related.limit}&related=${related.itemId}`;
+      let options = this.ajaxOptions(url, 'GET', {});
+      return wrapAjax(options);
+    } else {
+      return this._super(...arguments);
+    }
+  },
+  ajaxOptions(url, method, hash) {
+    hash = hash || {};
+    hash.crossDomain = true;
+    hash.xhrFields = {
+      withCredentials: true
+    };
+    return this._super(url, method, hash);
+  }
 });
