9ae396ed6c4207505d69b48a9eb3057b15981954
Jeff Keen <jeff@keen.me>
2017-08-30 17:08:19 -0500

    Move styles over from client

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,10 @@
-/* eslint-env node */
-'use strict';
-
-const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+/*jshint node:true*/
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {
-    // Add options here
+  var app = new EmberAddon(defaults, {
+
   });
 
   /*
