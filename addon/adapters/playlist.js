e30d6a4b615a61402dc1cf7dcaea4bec8bad1761
Brian Whitton <me@brianwhitton.com>
2017-09-17 22:31:08 -0400

    simplify playlist find records

--- addon/adapters/playlist.js
+++ addon/adapters/playlist.js
@@ -1,13 +1,11 @@
 import ENV from 'ember-get-config';
 import DS from 'ember-data';
-import wrapAjax from 'nypr-publisher-lib/utils/wrap-ajax';
 
 export default DS.JSONAPIAdapter.extend({
   host: ENV.wnycAPI,
   namespace: 'api/v1',
-  findRecord(store, type, id) {
-    let playlistUrl = `${this.host}/${this.namespace}/stream_playlist/${id}/`;
-    let options = this.ajaxOptions(playlistUrl, 'GET', {});
-    return wrapAjax(options);
+  pathForType: () => 'stream_playlist',
+  buildURL() {
+    return this._super(...arguments) + '/';
   }
 });
