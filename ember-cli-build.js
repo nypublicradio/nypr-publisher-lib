073ae718bef736dadbca2762d54d471a1d26f45d
Brian Whitton <me@brianwhitton.com>
2017-09-20 16:59:17 -0400

    remove comment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
     snippetSearchPaths: ['app', 'tests/dummy/app', 'addon'],
   });
 
