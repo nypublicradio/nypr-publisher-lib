40d33e1991cc4aacd921f1fd773427bc0aeeb551
Brian Whitton <me@brianwhitton.com>
2017-09-28 15:47:07 -0400

    adds about-page component write up

--- tests/dummy/app/routes/components/listing-pages/content-types.js
+++ tests/dummy/app/routes/components/listing-pages/content-types.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
+import {
+  DUMMY_STORY_1,
+  DUMMY_STORY_2,
+  ABOUT_PAGE
+ } from '../../../fixtures';
+
 
 export default Ember.Route.extend({
+  model() {
+    // BEGIN-SNIPPET about-page-api-response
+    let aboutApiResponse = this.store.createRecord('api-response', {
+      id: 'shows/bl/about/1',
+      aboutPage: this.store.createRecord('about-page', ABOUT_PAGE)
+    });
+    // END-SNIPPET
+    
+    return {
+      aboutApiResponse,
+    };
+  }
 });
