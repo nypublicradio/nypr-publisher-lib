9c61bd11e36145de170276997f1e38f37003a0f6
Brian Whitton <me@brianwhitton.com>
2017-09-28 15:49:04 -0400

    need imagesloaded to run demo app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,6 +15,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import('bower_components/imagesloaded/imagesloaded.pkgd.js');
 
   return app.toTree();
 };
