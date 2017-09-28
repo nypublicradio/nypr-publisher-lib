ed578bd75a9e4261aacf02af1d1f404d41e00a25
Brian Whitton <me@brianwhitton.com>
2017-09-28 15:45:22 -0400

    make fixture file for dummy data

---
+++ tests/dummy/app/fixtures.js
@@ -0,0 +1,139 @@
+export const DUMMY_STORY_1 = {
+  audio: 'https://www.podtrac.com/pts/redirect.mp3/audio.wnyc.org/bl/bl051914bpod.mp3',
+  audioAvailable: true,
+  audioDurationReadable: '1 hr',
+  cmsPK: 371424,
+  editLink: "cms/segment/371424",
+  headers: {
+    "brand": {
+      "url": "http://www.wnyc.org/shows/bl",
+      "title": "The Brian Lehrer Show",
+      "slug": "bl",
+    },
+    "links": [{
+      "url": "http://localhost:4567/blogs/scrapbook",
+      "title": "Huh. (The Brian Lehrer Show Blog)",
+      "item-type": "blog",
+      "slug": "scrapbook"
+    },{
+      "url": "http://www.wnyc.org/shows/bl",
+      "title": "The Brian Lehrer Show",
+      "itemType": "show",
+      "slug": "bl"
+    }]
+  },
+  imageMain: {
+    "crop": "l",
+    "template": "https://media2.wnyc.org/i/%s/%s/%s/%s/1/13958409859_774dfdc534_b.jpg",
+  },
+  newsdate: "2014-05-19T00:00:00-04:00",
+  tease: "When the owner of reBar, a DUMBO gastropub and wedding venue, disappeared two weeks ago with upwards of $150,000 in deposits, couples and employees were left in the lurch. The restaurateur has since turned himself in and paid bail but now faces up to 15 years in prison.",
+  title: "reBar: Closed and Bankrupt",
+  url: "http://www.wnyc.org/story/rebar/",
+};
+
+export const DUMMY_STORY_2 = {
+  audio: 'https://www.podtrac.com/pts/redirect.mp3/audio.wnyc.org/otm/otm051917pod.mp3',
+  audioAvailable: true,
+  audioDurationReadable: '1 hr',
+  body: "<p>The Trump-Comey story is largely missing from the far right-wing media. A look at how pro-Trump outlets choose to cover, or ignore, unfavorable news. Plus: the Montana special election has been described as a \"referendum\" on Trump... but the truth is actually more interesting. And we hear from a reporter who is training citizen journalists in Syria to cover life, not just war.</p>",
+  cmsPK: 757398,
+  editLink: "cms/episode/757398",
+  headers: {
+    "brand": {
+      "url": "http://wnyc.demo2.wnyc.net/shows/otm",
+      "title": "On The Media",
+      "slug": "otm",
+      "logo-image": {
+        "credits-url": "",
+        "name": "1/onthemedia.png",
+        "source": null,
+        "url": "https://media.demo.nypr.digital/i/500/500/c/80/1/onthemedia.png",
+        "h": 500,
+        "is-display": false,
+        "crop": "c",
+        "caption": "",
+        "template": "https://media.demo.nypr.digital/i/%s/%s/%s/%s/1/onthemedia.png",
+        "w": 500,
+        "id": 103448,
+        "credits-name": ""
+      }
+    },
+    "links": [{
+      "url": "http://wnyc.demo2.wnyc.net/shows/otm",
+      "title": "On The Media",
+      "item-type": "show",
+      "slug": "otm"
+    }]
+  },
+  imageMain: {
+    "credits-url": "",
+    "name": "1/comey_lCCw63M.jpg",
+    "source": {
+      "url": "",
+      "name": "Associated Press"
+    },
+    "url": "https://media.demo.nypr.digital/i/1860/1173/l/80/1/comey_lCCw63M.jpg",
+    "h": 1173,
+    "is-display": true,
+    "crop": "l",
+    "caption": "FBI Director James Comey listens during a news conference announcing a deal between the U.S. government and French bank BNP Paribas at the Justice Department in Washington, Monday, June 30, 2014.",
+    "template": "https://media.demo.nypr.digital/i/%s/%s/%s/%s/1/comey_lCCw63M.jpg",
+    "w": 1860,
+    "id": 164737,
+    "credits-name": "Susan Walsh"
+    },
+  newsdate: "2017-05-19T00:00:00-04:00",
+  tease: "Why the Trump-Comey story is largely missing from the right-wing media. Plus, what we can learn from Montana's special election, and training citizen journalists in Syria.",
+  title: "Curtains!",
+  url: "http://wnyc.demo2.wnyc.net/story/on-the-media-2017-05-19/"
+};
+
+// BEGIN-SNIPPET about-page
+export const ABOUT_PAGE = {
+  "body": "<p>Brian Lehrer leads the conversation about what matters most now in local and national politics, our own communities and our lives.</p>",
+  "people": [{
+    "url": "/people/brian-lehrer/",
+    "job-title": "Host, The Brian Lehrer Show",
+    "image": {
+      "credits-url": "",
+      "name": "photologue/photos/brian2_630x365.jpg",
+      "source": null,
+      "url": "https://media.demo.nypr.digital/i/630/365/c/80/photologue/photos/brian2_630x365.jpg",
+      "h": 365,
+      "is-display": false,
+      "crop": "c",
+      "caption": "Brian Lehrer",
+      "template": "https://media.demo.nypr.digital/i/%s/%s/%s/%s/photologue/photos/brian2_630x365.jpg",
+      "w": 630,
+      "id": 973,
+      "credits-name": "MarcoAntonio.com"
+    },
+    "name": "Brian Lehrer",
+    "social": [
+      {
+        "contact-string": "@BrianLehrer",
+        "service": "twitter"
+      },
+      {
+        "contact-string": "",
+        "service": "facebook"
+      },
+      {
+        "contact-string": "",
+        "service": "instagram"
+      }
+    ]
+  }],
+  "social": [{
+    "contact-string": "@TheBrianLehrerShow",
+    "service": "twitter"
+  }, {
+    "contact-string": "http://facebook.com/blshow",
+    "service": "facebook"
+  }, {
+    "contact-string": "@TheBrianLehrerShow",
+    "service": "instagram"
+  }]
+};
+// END-SNIPPET
