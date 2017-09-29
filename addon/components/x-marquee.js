4f5c4465d5cfddabb2e5bf526f8efb64ed0c394c
Brian Whitton <me@brianwhitton.com>
2017-09-28 23:18:46 -0400

    refactor `x-marquee` using computed properties

--- addon/components/x-marquee.js
+++ addon/components/x-marquee.js
@@ -1,21 +1,32 @@
 import Component from 'ember-component';
+import computed, { reads } from 'ember-computed';
 import { imageTemplate } from 'nypr-ui/helpers/image-template';
 import { htmlSafe } from 'ember-string';
 import get from 'ember-metal/get';
-import set from 'ember-metal/set';
 import layout from '../templates/components/x-marquee';
 
 export default Component.extend({
   layout,
   classNames: ['marquee'],
-  init() {
-    this._super(...arguments);
-
-    let bgColor = get(this, 'model.bgColor') || 'white';
-    let marquee = get(this, 'model.marqueeImage');
-    let urlString = imageTemplate([get(marquee, 'template'), 1200,  200, get(marquee, 'crop'), 99]);
-
-    set(this, 'marqueeImageCSS', htmlSafe(`background: url(${urlString}) no-repeat center center ${bgColor};`));
-    set(this, 'marqueeGradientCSS', htmlSafe(`background-image: radial-gradient(rgba(255, 255, 255,0) 200px, ${bgColor} 620px);`));
-  }
+  bgColor: reads('model.bgColor'),
+  marquee: reads('model.marqueeImage'),
+  marqueeURL: computed('marquee', function() {
+    let marquee = get(this, 'marquee');
+    if (!marquee) {
+      return;
+    }
+    return imageTemplate([get(marquee, 'template'), 1200,  200, get(marquee, 'crop'), 99]);
+  }),
+  marqueeImageCSS: computed('bgColor', 'marqueeURL', function() {
+    let marqueeURL = get(this, 'marqueeURL');
+    if (!marqueeURL) {
+      return;
+    }
+    let bgColor = get(this, 'bgColor') || 'white';
+    return htmlSafe(`background: url(${marqueeURL}) no-repeat center center ${bgColor};`)
+  }),
+  marqueeGradientCSS: computed('bgColor', function() {
+    let bgColor = get(this, 'bgColor') || 'white';
+    return htmlSafe(`background-image: radial-gradient(rgba(255, 255, 255,0) 200px, ${bgColor} 620px);`);
+  })
 });
