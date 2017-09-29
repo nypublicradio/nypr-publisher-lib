3d64792dc526c267a8263aebfd588a7569cb4c56
Brian Whitton <me@brianwhitton.com>
2017-09-28 22:30:58 -0400

    don't need this to be an id anymore

--- addon/components/channel-header.js
+++ addon/components/channel-header.js
@@ -6,7 +6,6 @@ export default Component.extend({
   layout,
 
   classNames:         ['channel-header'],
-  elementId:          'channel-header',
   subscribeOptions:   readOnly('model.podcastLinks'),
   donationOption:     readOnly('model.donate'),
   airings:            readOnly('model.scheduleSummary')
