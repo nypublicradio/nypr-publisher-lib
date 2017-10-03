c770e523575502815d767469ecf2c97c3eb7527a
Brian Whitton <me@brianwhitton.com>
2017-10-03 12:18:39 -0400

    move this data to the route

--- tests/dummy/app/routes/components/listing-pages/nav-links.js
+++ tests/dummy/app/routes/components/listing-pages/nav-links.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      // BEGIN-SNIPPET nav-links-params
+      navRoot: 'components.listing-pages.nav-links',
+      linkroll: [{
+        'nav-slug': 'foo',
+        title: 'More Foo'
+      }, {
+        'nav-slug': 'bar',
+        title: 'A List of Bar'
+      }, {
+        href: 'http://www.wnyc.org/about',
+        title: 'About WNYC'
+      }]
+      // END-SNIPPET
+    }
+  }
 });
