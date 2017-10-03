2e3ba7a901bdef3d15e3bcfba79f076cd57b91b3
Brian Whitton <me@brianwhitton.com>
2017-10-03 19:34:18 -0400

    cleans up stream-playlist component a bit

--- addon/components/stream-playlist.js
+++ addon/components/stream-playlist.js
@@ -1,17 +1,23 @@
 import Component from 'ember-component';
 import computed, { readOnly } from 'ember-computed';
-import service from 'ember-service/inject';
 import get from 'ember-metal/get';
+import { A } from 'ember-array/utils';
 import layout from '../templates/components/stream-playlist';
+
+const HOMEPAGES = {
+  // These don't seem to be part of an api
+  // Putting them here for now since this is the only component that uses them
+  'wqxr':             'http://www.wqxr.org',
+  'q2':               'http://www.wqxr.org/series/q2/',
+  'wqxr-special':     'http://www.wqxr.org/blogs/operavore/',
+  'jonathan-channel': 'http://www.wnyc.org/series/jonathan-channel'
+}
 
 export default Component.extend({
   layout,
-  store: service(),
   stream: null,
   homepage: computed('stream', function() {
-    let homepages = get(this, 'homepageList');
-    let slug = get(this, 'stream.id');
-    return homepages[slug];
+    return HOMEPAGES[get(this, 'stream.id')]
   }),
   currentItem: readOnly('stream.currentPlaylistItem.playlistEntryId'),
   historyItems: computed('stream.playlist.items.[]', 'currentItem', function() {
@@ -20,17 +26,13 @@ export default Component.extend({
     if (!playlist) {
       return [];
     }
-    return playlist.filter(i => Date.now() / 1000 > i.startTimeTs)
-      .rejectBy('playlistEntryId', currentId)
+
+    // ember's A does not return an ember array
+    let historyItems = A(playlist.filter(i => Date.now() / 1000 > i.startTimeTs))
+      .rejectBy('playlistEntryId', currentId);
+
+    return A(historyItems)
       .sortBy('startTimeTs')
       .reverse();
   }),
-  homepageList: {
-    // These don't seem to be part of an api
-    // Putting them here for now since this is the only component that uses them
-    'wqxr':             'http://www.wqxr.org',
-    'q2':               'http://www.wqxr.org/series/q2/',
-    'wqxr-special':     'http://www.wqxr.org/blogs/operavore/',
-    'jonathan-channel': 'http://www.wnyc.org/series/jonathan-channel'
-  }
 });
