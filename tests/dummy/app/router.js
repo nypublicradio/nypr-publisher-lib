6b815f5edb8ccb21c00f90a89c4d71dc7b951541
Brian Whitton <me@brianwhitton.com>
2017-10-03 12:21:24 -0400

    move this file around so the snippets look better for `nav-links`

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -12,18 +12,23 @@ Router.map(function() {
     this.route('adapters');
     this.route('serializers');
   });
+  // BEGIN-SNIPPET nav-links-router
   this.route('components', function() {
+    this.route('listing-pages', function() {
+      this.route('nav-links', function() {
+        // dummy routes for nav-links component
+        this.route('page', {path: '*'});
+      });
+      // END-SNIPPET
+      this.route('content-types');
+      this.route('channel-header');
+      this.route('x-marquee');
+      this.route('social-links');
+    });
     this.route('stories', function() {
       this.route('story-tease');
       this.route('story-comments');
       this.route('related-stories');
-    });
-    this.route('listing-pages', function() {
-      this.route('content-types');
-      this.route('channel-header');
-      this.route('nav-links');
-      this.route('x-marquee');
-      this.route('social-links');
     });
     this.route('streams');
   });
