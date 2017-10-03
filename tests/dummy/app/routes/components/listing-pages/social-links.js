6e323af5630e43f7796dc0e702d9c15e577d34c2
Brian Whitton <me@brianwhitton.com>
2017-10-03 13:16:08 -0400

    adds docs for social-links

--- tests/dummy/app/routes/components/listing-pages/social-links.js
+++ tests/dummy/app/routes/components/listing-pages/social-links.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return (
+      // BEGIN-SNIPPET social-links-data
+      [{
+        href: "https://www.facebook.com/BrianLehrerWNYC",
+        title: "facebook"
+      }, {
+        href: "https://twitter.com/BrianLehrer",
+        title: "twitter"
+      }, {
+        href: "http://nypublicradio.us5.list-manage1.com/subscribe?u=4109fdd323aaac7078eadaa8f&id=4ffeb93090",
+        title: "newsletter"
+      }]
+      // END-SNIPPET
+    );
+  }
 });
