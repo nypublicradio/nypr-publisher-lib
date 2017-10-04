24ad259c8014aaaeece29c60a8e9a17fe9ff5e4a
Brian Whitton <me@brianwhitton.com>
2017-10-04 10:39:02 -0400

    fix hide zero minutes test

--- tests/integration/helpers/hide-zero-minutes-test.js
+++ tests/integration/helpers/hide-zero-minutes-test.js
@@ -1,17 +1,25 @@
-
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('hide-zero-minutes', 'helper:hide-zero-minutes', {
   integration: true
 });
 
-// Replace this with your real tests.
-test('it renders', function(assert) {
-  this.set('inputValue', '1234');
+test('it hides the minutes place if minutes are zero', function(assert) {
+  let date = moment().set({hours: 10, minutes: 0});
+  this.set('inputValue', date);
 
   this.render(hbs`{{hide-zero-minutes inputValue}}`);
 
-  assert.equal(this.$().text().trim(), '1234');
+  assert.equal(this.$().text().trim(), '10 am');
 });
 
+test('it shows the minutes place if minutes are not zero', function(assert) {
+  let date = moment().set({hours: 10, minutes: 10});
+  this.set('inputValue', date);
+
+  this.render(hbs`{{hide-zero-minutes inputValue}}`);
+
+  assert.equal(this.$().text().trim(), '10:10 am');
+});
