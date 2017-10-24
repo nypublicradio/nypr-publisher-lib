3bcaf97ecc2d02bda9aa56eb565fccf2485b9cd9
walsh9 <walsh9@gmail.com>
2017-10-24 17:06:13 -0400

    Update episode list test

--- tests/integration/components/episode-list-test.js
+++ tests/integration/components/episode-list-test.js
@@ -6,19 +6,8 @@ moduleForComponent('episode-list', 'Integration | Component | episode list', {
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{episode-list}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#episode-list}}
-      template block text
-    {{/episode-list}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
