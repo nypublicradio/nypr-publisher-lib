43adc44241321ad8634ba43af72c28c5a089a499
walsh9 <walsh9@gmail.com>
2017-11-29 15:19:36 -0500

    Add test for episode list

--- tests/integration/components/episode-list-test.js
+++ tests/integration/components/episode-list-test.js
@@ -11,3 +11,27 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), '');
 });
+
+test('it shows episode teases', function(assert) {
+  let model = {
+    teaseList: [
+      {
+        title: "abc",
+        slug: "abc",
+        tease: "hello world",
+        newsdate: "2017-05-19T00:00:00-04:00"
+      },
+      {
+        title: "def",
+        slug: "def",
+        tease: "testing 123",
+        newsdate: "2017-05-19T00:00:00-04:00"
+      }
+    ]
+  };
+  this.set('model', model)
+
+  this.render(hbs`{{episode-list model=model}}`);
+
+  assert.equal(this.$('.episode-list__list-item').length, 2, 'it should show two episodes');
+});
