858bdbebe96f55be5185789292423ef964bef5cb
Brian Whitton <me@brianwhitton.com>
2017-11-30 09:05:41 -0500

    maybe the bower key needs to be specified?

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 module.exports = {
-  useYarn: true,
   scenarios: [
     {
       name: 'ember-lts-2.12',
@@ -64,6 +63,9 @@ module.exports = {
       name: 'ember-default',
       npm: {
         devDependencies: {}
+      },
+      bower: {
+        dependencies: {}
       }
     }
   ]
