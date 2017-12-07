5c3064574affb93fb7c761ffacad4b9d47df521c
Michael Hearn <git@mikehearn.net>
2017-12-07 10:58:22 -0500

    Check for bucket items and add fake id if empty

--- addon/serializers/bucket.js
+++ addon/serializers/bucket.js
@@ -2,17 +2,19 @@ import JSONAPISerializer from 'ember-data/serializers/json-api';
 import { camelize } from 'ember-string';
 
 export default JSONAPISerializer.extend({
-  extractId: (modelClass, {attributes}) => attributes.slug,
+  extractId: (modelClass, {attributes}) => attributes.slug || "none",
   // BEGIN-SNIPPET bucket-serializer
   normalizeResponse(store, klass, payload/*, id, requestType*/) {
     // these are not actual ember models; need to camelize for consumption by components
-    payload.data.attributes['bucket-items'].forEach(item => {
-      let { attributes } = item;
-      item.attributes = {};
+    if (payload.data.attributes['bucket-items']) {
+      payload.data.attributes['bucket-items'].forEach(item => {
+        let { attributes } = item;
+        item.attributes = {};
 
-      Object.keys(attributes)
-        .forEach(k => item.attributes[camelize(k)] = attributes[k]);
-    });
+        Object.keys(attributes)
+          .forEach(k => item.attributes[camelize(k)] = attributes[k]);
+      });
+    }
     return this._super(...arguments);
   }
   // END-SNIPPET
