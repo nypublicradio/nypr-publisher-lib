fc1a2acf01f2e70728741d307ee7525c4bdbb543
walsh9 <walsh9@gmail.com>
2018-03-16 16:54:06 -0400

    Camelize story attributes the same everywhere

--- addon/serializers/api-response.js
+++ addon/serializers/api-response.js
@@ -1,16 +1,19 @@
 import DS from 'ember-data';
 import { A } from '@ember/array';
+import { serializeStoryAttributes } from 'nypr-publisher-lib/serializers/story';
 
 export function serializeApiResponseRelationships(relationships = {}, included = []) {
   if (relationships['tease-list'] && relationships['tease-list'].data.length) {
     relationships['tease-list'].data.forEach(story => {
       let { attributes } = A(included).findBy('attributes.cms-pk', Number(story.id));
       story.id = attributes.slug;
+      serializeStoryAttributes(attributes);
     });
   }
   if (relationships.story && relationships.story.data) {
     let { attributes } = A(included).findBy('attributes.cms-pk', Number(relationships.story.data.id));
     relationships.story.data.id = attributes.slug;
+    serializeStoryAttributes(attributes);
   }
   return relationships;
 }
