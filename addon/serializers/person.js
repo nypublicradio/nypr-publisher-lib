6e25de05a57b48171ea5e6c1778b6da41cae45b0
Jeff Keen <jeff@keen.me>
2018-03-31 14:46:24 -0500

    Add extractId to show serializer to silence warning about using findRecord

--- addon/serializers/person.js
+++ addon/serializers/person.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  extractId: (modelClass, {attributes}) => attributes.slug,
+  extractId: (modelClass, {attributes}) => attributes.slug
 });
--- addon/serializers/show.js
+++ addon/serializers/show.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  extractId: (modelClass, {attributes}) => attributes.slug
 });
