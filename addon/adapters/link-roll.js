2c55b84521087d3601c0bde94392874ff930d441
walsh9 <walsh9@gmail.com>
2018-04-06 17:00:39 -0400

    add trailing slash, use slug as id

--- addon/adapters/link-roll.js
+++ addon/adapters/link-roll.js
@@ -4,5 +4,9 @@ import config from 'ember-get-config';
 export default DS.JSONAPIAdapter.extend({
   host: config.publisherAPI,
   namespace: 'v3',
-  pathForType: () => 'link-roll'
+  pathForType: () => 'link-roll',
+  buildURL() {
+    let url = this._super(...arguments);
+    return url.replace(/([^/])$/, '$1/');
+  },
 });
