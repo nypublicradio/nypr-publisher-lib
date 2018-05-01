639c4c2e2b3117e6f8838e6e1c7b7d5abf16af1e
Brian Whitton <me@brianwhitton.com>
2018-05-01 17:38:05 -0400

    add analytics bindings to this component

--- addon/components/show-tease.js
+++ addon/components/show-tease.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../templates/components/show-tease';
 
 export default Component.extend({
-  layout
+  layout,
+  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value']
 });
