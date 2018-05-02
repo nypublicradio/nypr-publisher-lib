a084e02a330cd8a04043b89f06da270068b73702
Brian Whitton <me@brianwhitton.com>
2018-05-02 11:25:58 -0400

    fix this test

--- tests/integration/components/show-tease-test.js
+++ tests/integration/components/show-tease-test.js
@@ -36,41 +36,38 @@ module('Integration | Component | show tease', function(hooks) {
     this.set("simpleLayout", false);
 
     await render(
-      hbs`{{show-tease show=testShow featuredSpot=featured trackingCategory="Test Category" simpleLayout=simpleLayout}}`
+      hbs`{{show-tease show=testShow featuredSpot=featured data-category="Test Category" simpleLayout=simpleLayout}}`
     );
 
-    assert.equal(this.$(".flag").hasClass("flag--med"), true, "Regular Show Tease has class flag-med");
-    assert.equal(this.$('.flag-body .text h4').text().trim(), 'WNYC and NPR', 'Producing orgs should render');
-    assert.equal(this.$('.flag-body .text h4 a').length, 0, 'Producing orgs should not have links');
-    assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
-    assert.equal(this.$('.flag-body .text .text-body--nopad').text().trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
-    assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/90/90/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
-    assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
-    assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
+    assert.equal(this.element.querySelector(".flag").classList.contains("flag--med"), true, "Regular Show Tease has class flag-med");
+    assert.equal(this.element.querySelector('.flag-body .text h4').textContent.trim(), 'WNYC and NPR', 'Producing orgs should render');
+    assert.notOk(this.element.querySelector('.flag-body .text h4 a'), 'Producing orgs should not have links');
+    assert.equal(this.element.querySelector('.flag-body .text h2').textContent.trim(), 'All Things Considered', 'Title should render correctly');
+    assert.equal(this.element.querySelector('.flag-body .text .text-body--nopad').textContent.trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
+    assert.equal(this.element.querySelector('.flag-image img').getAttribute("src"), "http://www.wnyc.org/i/90/90/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+    assert.equal(this.element.querySelector('[data-category]').getAttribute("data-category"), "Test Category", "Tracking Category should render on title link");
 
 
     // check for edits when it is a featured item
     this.set("featured", true);
 
-    assert.equal(this.$(".flag").hasClass("flag--luxe"), true, "Featured Spot has class flag-luxe");
-    assert.equal(this.$('.flag-body .text h4.text--lightgray').text().trim(), 'WNYC and NPR', 'Producing orgs should render');
-    assert.equal(this.$('.flag-body .text h4 a').length, 0, 'Producing orgs should not have links');
-    assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
-    assert.equal(this.$('.flag-body .text .text--default').text().trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
-    assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/150/150/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
-    assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
-    assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
+    assert.equal(this.element.querySelector(".flag").classList.contains("flag--luxe"), true, "Featured Spot has class flag-luxe");
+    assert.equal(this.element.querySelector('.flag-body .text h4.text--lightgray').textContent.trim(), 'WNYC and NPR', 'Producing orgs should render');
+    assert.notOk(this.element.querySelector('.flag-body .text h4 a'), 'Producing orgs should not have links');
+    assert.equal(this.element.querySelector('.flag-body .text h2').textContent.trim(), 'All Things Considered', 'Title should render correctly');
+    assert.equal(this.element.querySelector('.flag-body .text .text--default').textContent.trim(), "A wrap-up of the day's news, with features and interviews.", 'Description should render correctly');
+    assert.equal(this.element.querySelector('.flag-image img').getAttribute("src"), "http://www.wnyc.org/i/150/150/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+    assert.equal(this.element.querySelector('[data-category]').getAttribute("data-category"), "Test Category", "Tracking Category should render on title link");
 
     // check for edits when using simple layout - no producing org or tease text
     this.set("featured", false);
     this.set("simpleLayout", true);
-    assert.equal(this.$(".flag").hasClass("flag--med"), true, "Regular Show Tease has class flag-med");
-    assert.equal(this.$('.flag-body .text h4').length, 0, 'Producing orgs should not render');
-    assert.equal(this.$('.flag-body .text h2').text().trim(), 'All Things Considered', 'Title should render correctly');
-    assert.equal(this.$('.flag-body .text .text-body--nopad').length, 0, 'Description not render');
-    assert.equal(this.$('.flag-image img').attr("src"), "http://www.wnyc.org/i/90/90/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
-    assert.equal(this.$('.flag-body .text h2 a').attr("data-tracking-category"), "Test Category", "Tracking Category should render on title link");
-    assert.equal(this.$('a.flag-image').attr("data-tracking-category"), "Test Category", "Tracking Category should render on image link" );
+    assert.equal(this.element.querySelector(".flag").classList.contains("flag--med"), true, "Regular Show Tease has class flag-med");
+    assert.notOk(this.element.querySelector('.flag-body .text h4'), 'Producing orgs should not render');
+    assert.equal(this.element.querySelector('.flag-body .text h2').textContent.trim(), 'All Things Considered', 'Title should render correctly');
+    assert.notOk(this.element.querySelector('.flag-body .text .text-body--nopad'), 'Description not render');
+    assert.equal(this.element.querySelector('.flag-image img').getAttribute("src"), "http://www.wnyc.org/i/90/90/l/85/1/2DQ_1400X1400_NoWNYCSTUDIOS_2.png", "Image is populated");
+    assert.equal(this.element.querySelector('[data-category]').getAttribute("data-category"), "Test Category", "Tracking Category should render on title link");
 
   });
 });
