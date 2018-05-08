f4fd66d736ada704208b8af1db356c064387167f
Brian Whitton <me@brianwhitton.com>
2018-05-08 16:42:16 -0400

    mostly not tracking things except for something on studios

--- addon/components/social-iconlist.js
+++ addon/components/social-iconlist.js
@@ -4,5 +4,6 @@ import layout from '../templates/components/social-iconlist';
 export default Component.extend({
   layout,
   tagName: "ul",
-  classNames: ["social-iconlist"]
+  classNames: ["social-iconlist"],
+  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value']
 });
--- mirage-support/factories/django-page.js
+++ mirage-support/factories/django-page.js
@@ -50,7 +50,7 @@ export default Factory.extend({
                 <div id="edit-story"></div>
                 </div>
                 <button class="btn btn--blue btn--large js-listen"
-                  data-id="${story.id}" data-category="Listen"
+                  data-id="${story.id}"
                   data-ember-component="listen-button.embedded"
                   data-ember-args='{ "itemPK": "${story.id}", "itemTitle": "${story.title}", "duration": "${story.audioDurationReadable}", "playContext": "story-header", "type": "blue-boss" }'>
                     <i class="fa fa-play icon--prefix--large"></i>Listen <span class="text--small dimmed">${story.audioDurationReadable}</span>
