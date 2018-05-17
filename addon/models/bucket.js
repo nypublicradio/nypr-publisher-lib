a21e9187d6520519c3f03098bce0175ef70379d6
walsh9 <walsh9@gmail.com>
2018-05-17 09:45:02 -0400

    Leave bucket's bucketItems as an attr() POJO for now

--- addon/models/bucket.js
+++ addon/models/bucket.js
@@ -1,12 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   // BEGIN-SNIPPET bucket-fields
   slug: attr('string'),
   title: attr('string'),
   editLink: attr('string'),
-  bucketItems: hasMany('bucketItem', {async: false, polymorphic: true, inverse: null})
+  bucketItems: attr()
   // END-SNIPPET
 });
