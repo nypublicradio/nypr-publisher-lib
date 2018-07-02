a9b063f8d382d605ee452655a0d86ef5187e42fe
Brian Whitton <me@brianwhitton.com>
2018-07-02 09:38:31 -0400

    Revert "mostly not tracking things except for something on studios"
    This reverts commit 788ece08cd56127fd4a3e4094b67cc4f6615e520.

--- addon/components/social-iconlist.js
+++ addon/components/social-iconlist.js
@@ -4,6 +4,5 @@ import layout from '../templates/components/social-iconlist';
 export default Component.extend({
   layout,
   tagName: "ul",
-  classNames: ["social-iconlist"],
-  attributeBindings: ['data-category', 'data-action', 'data-label', 'data-value']
+  classNames: ["social-iconlist"]
 });
