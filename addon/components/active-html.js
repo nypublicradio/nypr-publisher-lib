3d1a4110dbae7cfa7006b5a9273b368cb5815c33
walsh9 <walsh9@gmail.com>
2018-07-27 15:39:47 -0400

    Make active-html fastboot safe

--- addon/components/active-html.js
+++ addon/components/active-html.js
@@ -1,17 +1,24 @@
 import Component from '@ember/component';
 import layout from '../templates/components/active-html';
+import { inject as service } from '@ember/service';
+import { schedule } from '@ember/runloop';
 import { computed } from '@ember/object';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
   html: "",
+  fastboot: service(),
+  isFastBoot: reads('fastboot.isFastBoot'),
   unescapedHTML: computed('html', function() {
      return this.get('html').replace(/\\x3C\/script>/g, '</script>');
   }),
-  didRender() {
+  didInsertElement() {
     let html = this.get('unescapedHTML');
-    let element = this.$();
-    element.empty();
-    element.append(html);
+    schedule('render', () => {
+      let element = this.$();
+      element.empty();
+      element.append(html);
+    });
   }
 });
