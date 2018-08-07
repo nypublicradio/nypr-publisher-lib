95fadb4e883c4f888df5c56d8dfa1ca263fcaefc
walsh9 <walsh9@gmail.com>
2018-08-07 11:36:26 -0400

    Make active-html pass tests better

--- addon/components/active-html.js
+++ addon/components/active-html.js
@@ -1,15 +1,12 @@
 import Component from '@ember/component';
 import layout from '../templates/components/active-html';
-import { inject as service } from '@ember/service';
 import { schedule } from '@ember/runloop';
 import { computed } from '@ember/object';
-import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
   html: "",
-  fastboot: service(),
-  isFastBoot: reads('fastboot.isFastBoot'),
+  isFastBoot: true,
   unescapedHTML: computed('html', function() {
      return this.get('html').replace(/\\x3C\/script>/g, '</script>');
   }),
