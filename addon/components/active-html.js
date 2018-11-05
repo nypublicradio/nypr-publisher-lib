470daa072d274921d3cf92edf01a37bfec6657fe
walsh9 <walsh9@gmail.com>
2018-11-05 12:15:46 -0500

    Update active-html to handle content changes

--- addon/components/active-html.js
+++ addon/components/active-html.js
@@ -6,15 +6,23 @@ import { computed } from '@ember/object';
 export default Component.extend({
   layout,
   html: "",
-  isFastBoot: true,
+  loading: true,
   unescapedHTML: computed('html', function() {
      return this.get('html').replace(/\\x3C\/script>/g, '</script>');
   }),
   didInsertElement() {
-    this.set('isFastBoot', false)
+    this.set('loading', false)
     let html = this.get('unescapedHTML');
     let element = this.$();
     // element.empty();
+    schedule('render', () => {
+      element.append(html);
+    });
+  },
+  didUpdateAttrs() {
+    let html = this.get('unescapedHTML');
+    let element = this.$();
+    element.empty();
     schedule('render', () => {
       element.append(html);
     });
