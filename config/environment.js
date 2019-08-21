13df9b093942c936eb237fc31ed5d0044408be4e
Jim Jazwiecki <jjazwiecki@nypublicradio.org>
2019-08-21 19:35:05 -0400

    switch to @fortawesome/ember-fontawesome

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
+  // TODO: use only social icons for smaller/faster builds
+  // let ENV = {
+  //   // Add options here
+  //   fontawesome: {
+  //     icons: {
+  //       'free-solid-svg-icons': 'all',
+  //       'pro-light-svg-icons': [
+  //         'adjust',
+  //         'ambulance',
+  //         'pencil-alt'
+  //       ]
+  //     }
+  //   }
+  // };
+  // return ENV;
   return { };
 };
---
+++ config/icons.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+  return {
+    'free-solid-svg-icons': ['envelope'],
+    'free-brands-svg-icons': ['facebook', 'twitter'],
+  };
+};
