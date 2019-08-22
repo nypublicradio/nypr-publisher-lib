fd83c3ce9cea062fe0595071963b768c3784b6a2
Jim Jazwiecki <jjazwiecki@nypublicradio.org>
2019-08-22 11:07:40 -0400

    only include 3 fontawesome svgs

--- config/environment.js
+++ config/environment.js
@@ -1,20 +1,16 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  // TODO: use only social icons for smaller/faster builds
-  // let ENV = {
-  //   // Add options here
-  //   fontawesome: {
-  //     icons: {
-  //       'free-solid-svg-icons': 'all',
-  //       'pro-light-svg-icons': [
-  //         'adjust',
-  //         'ambulance',
-  //         'pencil-alt'
-  //       ]
-  //     }
-  //   }
-  // };
-  // return ENV;
-  return { };
+  let ENV = {
+    fontawesome: {
+      icons: {
+        'free-solid-svg-icons': 'envelope',
+        'free-brands-svg-icons': [
+          'facebook',
+          'twitter'
+        ]
+      }
+    }
+  };
+  return ENV;
 };
