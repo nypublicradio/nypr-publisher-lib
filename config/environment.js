937509a7ee7119f4c27cd1dc503104c9788ff9bb
Jim Jazwiecki <jjazwiecki@nypublicradio.org>
2019-08-22 12:03:31 -0400

    maybe don't just include the icons we use? idk.

--- config/environment.js
+++ config/environment.js
@@ -1,16 +1,19 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  let ENV = {
-    fontawesome: {
-      icons: {
-        'free-solid-svg-icons': 'envelope',
-        'free-brands-svg-icons': [
-          'facebook',
-          'twitter'
-        ]
-      }
-    }
-  };
-  return ENV;
+  // TODO: use only social icons for smaller/faster builds
+  // let ENV = {
+  //   // Add options here
+  //   fontawesome: {
+  //     icons: {
+  //       'free-solid-svg-icons': 'envelope',
+  //       'free-brands-svg-icons': [
+  //         'facebook',
+  //         'twitter'
+  //       ]
+  //     }
+  //   }
+  // };
+  // return ENV;
+  return { };
 };
