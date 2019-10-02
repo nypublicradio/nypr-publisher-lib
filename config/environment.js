5214e6617f1f6980158d3d67d8a5a20728f5ddb0
Jim Jazwiecki <jjazwiecki@nypublicradio.org>
2019-10-02 14:08:26 -0400

    upgrade ember fontawesome (#56)
    * switch to @fortawesome/ember-fontawesome

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
+  // @todo use only social icons for smaller/faster builds
+  // @body this library only uses envelope, fb, and twitter.
+  //       see https://github.com/FortAwesome/ember-fontawesome#subsetting-icons
   return { };
 };
