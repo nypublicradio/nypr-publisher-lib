9fc43b6bda8b61b0e0e264ddbe861e74becf92d2
Brad Kammin <bkammin@nypublicradio.org>
2019-12-13 10:42:19 -0500

    DSODA-94 add `contactEmail` to `Show` model

--- addon/models/show.js
+++ addon/models/show.js
@@ -4,6 +4,7 @@ import ChannelModel from 'nypr-publisher-lib/models/channel';
 export default ChannelModel.extend({
   // BEGIN-SNIPPET show-fields
   image: DS.belongsTo('image'),
-  producingOrganizations: DS.hasMany('producing-organization')
+  producingOrganizations: DS.hasMany('producing-organization'),
+  contactEmail: DS.attr('string')
   // END-SNIPPET
 });
